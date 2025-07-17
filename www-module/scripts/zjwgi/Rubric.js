080e7a85228209d58d5f7f0750b0da12ae4288a8
Zachary Wartell <zjayuu@gmail.com>
2025-07-17 18:39:03 -0400

    -bug fix : try #3, integrationg new data-zxw-mvc='dynamic-content-self

--- www-module/scripts/zjwgi/Rubric.js
+++ www-module/scripts/zjwgi/Rubric.js
@@ -636,7 +636,7 @@ export class Instructions {
                 }
                 else
                     //iElement.insertAdjacentHTML("afterbegin","|<sup>"+ptDiv.innerHTML + "</sup>| &nbsp;");
-                    iElement.insertAdjacentHTML("afterbegin", "|<sup>" + ptDiv.innerHTML + "</sup>| &nbsp;");
+                    iElement.insertAdjacentHTML("afterbegin", "|<sup>" + ptDiv.innerHTML + "</sup>|");
             }
             /**
              *   In the Rubric table, insert rows corresponding to all Instructions
